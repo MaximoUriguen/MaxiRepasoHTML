e339554f7992571af9a25dd6c1ee5b3932d970e8
alumno <alumno@Instituto.local>
2025-04-01 09:47:27 -0300

    js un poquito
    sasa

---
+++ java.js
@@ -0,0 +1,10 @@
+//seleccionar un elemento document.querySelector()
+let boton = document.querySelector('#EsteCambiaButton') 
+let parrafo = document.querySelector('#EsteCambia')
+
+boton.onclick = function () {
+    parrafo.style.color = 'red'
+}
+
+
+    
